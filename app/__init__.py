3a0840c9f1ad7e4373fa404efb7a666a396523d9
Vezer917 <tredemptation@hotmail.com>
2020-07-16 17:56:17 -0700

    fixing missing module errors in heroku

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-from config import Config
+from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
