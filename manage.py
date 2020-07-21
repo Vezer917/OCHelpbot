3319dc936ea68f45a84d75de1b19817abb16e50b
Vezer917 <tredemptation@hotmail.com>
2020-07-21 15:15:57 -0700

    added manage.py

---
+++ manage.py
@@ -0,0 +1,16 @@
+# manage.py
+
+from flask_script import Manager
+
+from app import app
+
+manager = Manager(app)
+
+
+@manager.command
+def hello():
+    print("hello")
+
+
+if __name__ == "__main__":
+    manager.run()
