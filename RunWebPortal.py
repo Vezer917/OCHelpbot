e39ee135d8c6d600affd001f716c473b4053cae0
Vezer917 <tredemptation@hotmail.com>
2020-07-21 14:51:03 -0700

    reworked procfile

--- RunWebPortal.py
+++ RunWebPortal.py
@@ -3,5 +3,5 @@ from app import app
 # Do not set debug=True when webportal is live or you could be exposing yourself to the horrors of the internet!
 
 
-def go():
-    app.run(debug=False)
+if __name__ == "__main__":
+    app.run()
