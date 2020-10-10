c6cc69ee75c8a5aff5382b9f72dad224e44319f6
Vezer917 <tredemptation@hotmail.com>
2020-10-10 11:17:22 -0700

    Possibly fixed the quiz command

--- cogs/quiz.py
+++ cogs/quiz.py
@@ -41,7 +41,7 @@ class Quiz(commands.Cog):
         channelname = ctx.channel.name.split('_')
         name = channelname[0]
         print(name)
-        c.execute(f"SELECT name, questions, madeby, score FROM quiz WHERE name='{str.lower(name)}';")
+        c.execute(f"SELECT name, questions, madeby, score FROM quiz WHERE name='{str(name)}' COLLATE NOCASE;")
         info = c.fetchone()
         if args is None:
             # If there is no course specified but you are in a course channel (ie 'cosc111_computer-programming')
@@ -56,16 +56,16 @@ class Quiz(commands.Cog):
                     description=desc,
                     color=0x206694
                 )
-                await ctx.send("I found a quiz for this channel: \n(Type '!quiz run' to start the quiz)")
+                await ctx.send("I found a quiz for this channel: \n(Type `!quiz run` to start the quiz)")
                 await ctx.send(embed=embed, content=None)
                 return
             # no quiz specified and not a course channel
 
-            await ctx.send("'!quiz [NAME]' to run a quiz\n"
-                           "'!quizlist' to see list of quizzes\n"
-                           "'!addquiz [NAME]' to add a quiz\n"
-                           "'!delquiz [NAME]' to delete a quiz\n"
-                           "'!addq [QUIZNAME]' to add a question to a quiz")
+            await ctx.send("`!quiz [NAME]` to run a quiz\n"
+                           "`!quizlist` to see list of quizzes\n"
+                           "`!addquiz [NAME]` to add a quiz\n"
+                           "`!delquiz [NAME]` to delete a quiz\n"
+                           "`!addq [QUIZNAME]` to add questions to a quiz")
             return
         # run quiz for current channel
         if args == 'run':
@@ -76,7 +76,7 @@ class Quiz(commands.Cog):
             return
         # clean the args to prevent SQL injection
         args = dbcon.sanitize(args)
-        c.execute(f"SELECT name FROM quiz WHERE name='{args}';")
+        c.execute(f"SELECT name FROM quiz WHERE name='{args}' COLLATE NOCASE;")
         quizinfo = c.fetchone()
         if quizinfo is None:
             await ctx.send("Quiz not found\nType '!quizlist' to see a list of all quizzes")
@@ -99,45 +99,53 @@ class Quiz(commands.Cog):
             await ctx.send('Please specify the quiz you are adding a question to\nEg: !addq cosc111')
             return
         # get quiz
-        c.execute("SELECT name, questions FROM quiz WHERE name='" + str(args) + "';")
+        c.execute(f"SELECT name, questions FROM quiz WHERE name='{str(args)}' COLLATE NOCASE;")
         quizname = c.fetchone()
         if quizname is None:
-            await ctx.author.dm_channel.send("No quiz by that name found. Type '!quizlist' to see a list of quizzes")
+            await ctx.author.dm_channel.send("No quiz by that name found. Type `!quizlist` to see a list of quizzes")
             return
-        await ctx.author.dm_channel.send("Please tell me the question:")
-        question = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author, timeout=60.0)
-        await ctx.author.dm_channel.send("Great, now the answer:")
-        answer = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author, timeout=60.0)
-        await ctx.author.dm_channel.send("Please review the question and type 'y' to accept or 'n' to cancel:")
-        desc = ""
-        desc += "\nQuestion: " + question.content
-        desc += "\nAnswer: " + answer.content
-        embed = discord.Embed(
-            title=quizname[0],
-            description=desc,
-            color=0x206694
-        )
-        await ctx.author.dm_channel.send(embed=embed, content=None)
-        confirm = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author, timeout=60.0)
-        if confirm.content.startswith('n') or confirm.content.startswith('N'):
-            await ctx.author.dm_channel.send("Cancelled")
-            return
-        else:
-            try:
-                newnumberofquestions = int(quizname[1]) + 1
-                sql = "INSERT INTO questions (quizname, question, answer, score) VALUES (?, ?, ?, ?)"
-                c.execute(sql, (quizname[0], question.content, answer.content, 50))
-                conn.commit()
-                sql = "UPDATE quiz SET questions=? WHERE name=?"
-                c.execute(sql, (newnumberofquestions, quizname[0]))
-            except sqlite3.Error as e:
-                print(type(e).__name__)
-            except:
-                await ctx.author.dm_channel.send("Hmmm.... something went wrong")
+        stop = False
+        while not stop:
+            await ctx.send("*Type `!end` at any time to exit*")
+            await ctx.author.dm_channel.send("Please tell me the question:")
+            question = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author, timeout=60.0)
+            if question.content == '!end':
+                return
+            await ctx.author.dm_channel.send("Great, now the answer:")
+            answer = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author, timeout=60.0)
+            if answer.content == '!end':
+                return
+            await ctx.author.dm_channel.send("Please review the question and type `y` to accept and add another or "
+                                             "`n` to cancel:")
+            desc = ""
+            desc += "\nQuestion: " + question.content
+            desc += "\nAnswer: " + answer.content
+            embed = discord.Embed(
+                title=quizname[0],
+                description=desc,
+                color=0x206694
+            )
+            await ctx.author.dm_channel.send(embed=embed, content=None)
+            confirm = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author, timeout=60.0)
+            if confirm.content.startswith('n') or confirm.content.startswith('N'):
+                await ctx.author.dm_channel.send("Cancelled")
                 return
             else:
-                conn.commit()
-                await ctx.author.dm_channel.send("Question added")
+                try:
+                    newnumberofquestions = int(quizname[1]) + 1
+                    sql = "INSERT INTO questions (quizname, question, answer, score) VALUES (?, ?, ?, ?)"
+                    c.execute(sql, (quizname[0], question.content, answer.content, 50))
+                    conn.commit()
+                    sql = "UPDATE quiz SET questions=? WHERE name=? COLLATE NOCASE"
+                    c.execute(sql, (newnumberofquestions, quizname[0]))
+                except sqlite3.Error as e:
+                    print(type(e).__name__)
+                except:
+                    await ctx.author.dm_channel.send("Hmmm.... something went wrong")
+                    return
+                else:
+                    conn.commit()
+                    await ctx.author.dm_channel.send("Question added")
         return
 
     @commands.command(name="quizlist")
@@ -162,15 +170,15 @@ class Quiz(commands.Cog):
     )
     async def makequiz(self, ctx, *, arg=None):
         # check to see if private channel exists, if not makes one
-        if ctx.author.dm_channel is None:
-            await ctx.author.create_dm()
+        # if ctx.author.dm_channel is None:
+        #    await ctx.author.create_dm()
         if arg is None:
-            await ctx.send("Please enter a name of the quiz\nEg: !makequiz Ken Trivia")
+            await ctx.send("Please enter a name of the quiz\nEg: `!makequiz Ken Trivia`")
             return
         # clean the arg to prevent SQL injection
         arg = dbcon.sanitize(arg)
         quizname = str(arg)
-        c.execute("SELECT name FROM quiz WHERE name='" + quizname + "';")
+        c.execute(f"SELECT name FROM quiz WHERE name='{quizname}' COLLATE NOCASE;")
         q = c.fetchone()
         if q is not None:
             await ctx.send("A quiz by that name already exists")
@@ -182,11 +190,11 @@ class Quiz(commands.Cog):
         except sqlite3.Error as e:
             print(type(e).__name__)
         except:
-            await ctx.author.dm_channel.send("Something went wrong adding the quiz\nQuizname: " + quizname +
-                                             "\nauthor: " + str(ctx.author))
+            await ctx.send("Something went wrong adding the quiz\nQuizname: " + quizname +
+                           "\nauthor: " + str(ctx.author))
             return
         else:
-            await ctx.author.dm_channel.send(
+            await ctx.send(
                 "Quiz created. Type '!addq " + quizname + "' to add questions to this quiz")
         return
 
@@ -244,12 +252,14 @@ async def run(self, ctx, args):
     if ctx.author.dm_channel is None:
         await ctx.author.create_dm()
     # grab quiz
+    print(args)
     c.execute(f"SELECT name, questions, madeby, score FROM quiz WHERE name='{args}' COLLATE NOCASE;")
     quizinfo = c.fetchone()
+    print(quizinfo[0])
     quizname = str(quizinfo[0])
     desc = ""
-    desc += "\nNumber of Questions: " + quizinfo[1]
-    desc += "\nMade by: " + quizinfo[2]
+    desc += "\nNumber of Questions: " + str(quizinfo[1])
+    desc += "\nMade by: " + str(quizinfo[2])
     embed = discord.Embed(
         title=quizname,
         description=desc,
@@ -257,14 +267,15 @@ async def run(self, ctx, args):
     )
     await ctx.send(embed=embed, content=None)
     # grab questions
-    c.execute("SELECT question, answer, score FROM questions WHERE quizname='" + args + "';")
+    c.execute(f"SELECT question, answer, score FROM questions WHERE quizname='{args}' COLLATE NOCASE;")
     questions = c.fetchall()
     totalscore = 0
     counter = 0
     for q in questions:
+        # async with ctx.typing():
         counter += 1
         msg = ""
-        await ctx.author.dm_channel.send("Question " + str(counter) + ":\n" + q[0])
+        await ctx.author.dm_channel.send(f"Question {str(counter)}:\n{q[0]}")
         try:
             msg = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author, timeout=60.0)
         except asyncio.TimeoutError:
@@ -277,16 +288,15 @@ async def run(self, ctx, args):
             await ctx.author.dm_channel.send('Correct!')
             totalscore += 1
         else:
-            await ctx.author.dm_channel.send('Incorrect. The correct answer was:\n**' + str(q[1]) + "**")
+            await ctx.author.dm_channel.send(f'Incorrect. The correct answer was:\n**{str(q[1])}**')
     scoremessage = ''
     outof = float(quizinfo[1])
-    if totalscore / outof >= 1:
+    if totalscore >= counter:
         scoremessage = '*Perfect score! Nice!*'
-    else:
-        if totalscore / outof >= .5:
-            scoremessage = '*Good job, you passed!*'
-        if totalscore / outof < .5:
-            scoremessage = '*Better luck next time!*'
+    if totalscore >= counter / 2:
+        scoremessage = '*Good job, you passed!*'
+    if totalscore < counter /2:
+        scoremessage = '*Better luck next time!*'
 
     await ctx.author.dm_channel.send(
         f'Quiz finished. *Total score: {str(totalscore)} out of {str(quizinfo[1])}*\n{scoremessage}')
